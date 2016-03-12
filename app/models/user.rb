acca3d371b38d38f691bb36eca0b8ed6fade8ec0
Dervol03 <b_d_v@web.de>
2016-03-12 20:36:13 +0100

    refs #1 : First Authentication
    * Moved password length validation to correct place
    * Added method for user to log out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 # Represents the general user of the application, independently from its later
 # role in an adventure.
 class User < ActiveRecord::Base
-  # Override default password length of devise's validation
-  Devise.password_length = 6..75
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise  :database_authenticatable, :rememberable, :trackable, :validatable
