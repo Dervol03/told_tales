4a76ecf30649a4ced4627fc2a9c8eef6bd275508
Dervol03 <b_d_v@web.de>
2016-03-13 07:10:42 +0100

    refs #2 : User Management
    * Updated existing migrations to have a unique user

--- db/migrate/20160306113157_create_users.rb
+++ db/migrate/20160306113157_create_users.rb
@@ -1,10 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :name, null: false
       t.string :password
 
       t.timestamps null: false
     end
+
+    add_index :users, :name, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@
 ActiveRecord::Schema.define(version: 20160306113158) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
+    t.string   "name",                                null: false
     t.string   "password"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20160306113158) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["name"], name: "index_users_on_name", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
