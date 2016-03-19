5066e71338e1b3ea6bc5766b340919afe69ee283
Dervol03 <b_d_v@web.de>
2016-03-19 07:46:55 +0100

    refs #4 : Add an Adventure
    * Fixed Adventure Specs

--- spec/views/adventures/edit.html.haml_spec.rb
+++ spec/views/adventures/edit.html.haml_spec.rb
@@ -1,24 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe "adventures/edit", type: :view do
+RSpec.describe 'adventures/edit', type: :view do
   before(:each) do
-    @adventure = assign(:adventure, Adventure.create!(
-      :name => "MyString",
-      :setting => "MyText",
-      :started => false
-    ))
+    @adventure = assign(:adventure, Fabricate(:adventure))
   end
 
-  it "renders the edit adventure form" do
+  it 'renders the edit adventure form' do
     render
 
-    assert_select "form[action=?][method=?]", adventure_path(@adventure), "post" do
+    assert_select 'form[action=?][method=?]', adventure_path(@adventure), 'post' do
 
-      assert_select "input#adventure_name[name=?]", "adventure[name]"
+      assert_select 'input#adventure_name[name=?]', 'adventure[name]'
 
-      assert_select "textarea#adventure_setting[name=?]", "adventure[setting]"
+      assert_select 'textarea#adventure_setting[name=?]', 'adventure[setting]'
 
-      assert_select "input#adventure_started[name=?]", "adventure[started]"
+      assert_select 'input#adventure_started[name=?]', 'adventure[started]'
     end
   end
 end
--- spec/views/adventures/show.html.haml_spec.rb
+++ spec/views/adventures/show.html.haml_spec.rb
@@ -1,18 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "adventures/show", type: :view do
+RSpec.describe 'adventures/show', type: :view do
   before(:each) do
-    @adventure = assign(:adventure, Adventure.create!(
-      :name => "Name",
-      :setting => "MyText",
-      :started => false
-    ))
+    @adventure = Fabricate(:adventure)
+    @adventure = assign(:adventure, @adventure)
   end
 
-  it "renders attributes in <p>" do
+  it 'renders attributes in <p>' do
     render
-    expect(rendered).to match(/Name/)
-    expect(rendered).to match(/MyText/)
-    expect(rendered).to match(/false/)
+    expect(rendered).to match(/#{@adventure.name}/)
+    expect(rendered).to match(/#{@adventure.setting}/)
+    expect(rendered).to match(/#{@adventure.started}/)
   end
 end
