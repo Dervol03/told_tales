1f7b863f3aebede4853e165cddd0e0a23c7882e1
Yves Komenda <b_d_v@web.de>
2016-04-08 00:02:36 +0200

    youtrack test 3
    refs #TT-1

--- spec/models/adventure_spec.rb
+++ spec/models/adventure_spec.rb
@@ -562,7 +562,6 @@ describe Adventure, type: :model do
   context 'teamcity' do
     it 'should be true' do
       expect(true).to be true
-      expect(true).to be true
     end
 
     it 'should be false' do
